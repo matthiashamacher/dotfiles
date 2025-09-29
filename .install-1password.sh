1fe912f0c82d9c2eb258262afc407961012947a1
Matthias Hamacher <m.hamacher@basecom.de>
2025-09-29 17:53:23 +0200

    Add 1password login

--- .install-1password.sh
+++ .install-1password.sh
@@ -8,3 +8,10 @@ brew install 1password
 
 # install 1password cli
 brew install 1password-cli
+
+# Login to 1password if not already logged in
+if [ ! -f "${HOME}/.config/chezmoi/.1passwordlogin" ]; then
+    op account add --address my.1password.eu
+    eval $(op signin)
+    op account list > "${HOME}/.config/chezmoi/.1passwordlogin"
+fi
