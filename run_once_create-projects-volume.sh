f7ee0382eeb7c746cb60b3875b59ff735d27a2e3
Matthias Hamacher <m.hamacher@basecom.de>
2025-10-07 19:05:57 +0200

    Fix duplicate creation

--- run_once_create-projects-volume.sh
+++ run_once_create-projects-volume.sh
@@ -3,5 +3,11 @@
 # get the container to create the volume with diskutil
 container=$(diskutil apfs list | grep -m 1 -E 'Container disk[0-9]+' | grep -E 'disk[0-9]' | awk '{print $3}')
 
+# Check if the volume already exists
+if diskutil apfs list | grep -q /Volumes/Projects; then
+    echo "Volume 'Projects' already exists. Skipping creation."
+    exit 0
+fi
+
 # Add volume
 diskutil apfs addVolume "$container" APFSX "Projects" -role D
