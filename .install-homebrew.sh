f16ca57c9a4f7db3d3a4e8329ef58bc113e1252c
Matthias Hamacher <devops+github@mhamacher.dev>
2025-10-11 16:08:02 +0200

    Update .install-homebrew.sh

--- .install-homebrew.sh
+++ .install-homebrew.sh
@@ -5,3 +5,5 @@ type brew > /dev/null 2>&1 && exit
 
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
