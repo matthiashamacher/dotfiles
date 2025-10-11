535c12d08a1aa87dd0a7dd9a3d9445435d1a39c1
Matthias Hamacher <devops+github@mhamacher.dev>
2025-10-11 15:06:32 +0200

    Change 1Password signin to use cache option

--- .install-1password.sh
+++ .install-1password.sh
@@ -14,6 +14,6 @@ source ~
 # Login to 1password if not already logged in
 if [ ! -f "${HOME}/.config/chezmoi/.1passwordlogin" ]; then
     op account add --address my.1password.eu
-    eval $(op signin)
+    eval $(op signin --cache)
     op account list > "${HOME}/.config/chezmoi/.1passwordlogin"
 fi
