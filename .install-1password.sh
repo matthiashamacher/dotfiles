2369e9bc3f3794adb4005611670322d27eb43079
Matthias Hamacher <devops+github@mhamacher.dev>
2025-10-11 16:08:23 +0200

    Update .install-1password.sh

--- .install-1password.sh
+++ .install-1password.sh
@@ -11,8 +11,6 @@ open -n /Applications/1Password.app
 # install 1password cli
 brew install 1password-cli
 
-source ~
-
 # Login to 1password if not already logged in
 if [ ! -f "${HOME}/.config/chezmoi/.1passwordlogin" ]; then
     op account add --address my.1password.eu
