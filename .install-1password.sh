21ca67464ddc47620ef182e48ddd8f90afa85696
Matthias Hamacher <devops+github@mhamacher.dev>
2025-10-21 08:00:24 +0200

    Add account to 1password signin

--- .install-1password.sh
+++ .install-1password.sh
@@ -14,6 +14,6 @@ open -n /Applications/1Password.app
 # Login to 1password if not already logged in
 if [ ! -f "${HOME}/.config/chezmoi/.1passwordlogin" ]; then
     op account add --address my.1password.eu
-    eval $(op signin --cache)
+    eval $(op signin --cache --account my.1password.eu)
     op account list > "${HOME}/.config/chezmoi/.1passwordlogin"
 fi
--- .signin-1password.sh
+++ .signin-1password.sh
@@ -3,5 +3,5 @@
 # Login to 1password if not already logged in
 if ! op whoami >/dev/null 2>&1; then
     echo "Signing in to 1Password..."
-    eval $(op signin --cache)
+    eval $(op signin --cache --account my.1password.eu)
 fi
