dcef325a718c8e0338fec68239583de846e1c986
Matthias Hamacher <devops+github@mhamacher.dev>
2026-02-28 21:36:15 +0100

    feat: add zerobrew installation and an export statement to the homebrew setup script.

--- .install-homebrew.sh
+++ .install-homebrew.sh
@@ -7,3 +7,8 @@ type brew > /dev/null 2>&1 && exit
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# install zerobrew
+curl -fsSL https://zerobrew.rs/install | bash
+
+export
