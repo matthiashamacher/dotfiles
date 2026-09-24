df8f61bb5bf13fd27e4b01cf9f2038a4fc35d2f3
Matthias Hamacher <devops+github@mhamacher.dev>
2026-09-24 22:22:47 +0200

    fix(setup): run homebrew installer non-interactively

--- .install-homebrew.sh
+++ .install-homebrew.sh
@@ -3,8 +3,11 @@
 # exit if homebrew is already installed
 type brew > /dev/null 2>&1 && exit
 
+# cache sudo credentials so the installer can run without the "Press RETURN" prompt
+sudo -v
+
 # install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
